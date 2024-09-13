5c1c1a50639f71a31127799f2e2262a42c8add12
Roberto Oyarzun <royarzun@integer.cl>
2024-09-12 22:22:21 -0300

    Actualización nashenta
    Añadido!
    
    - Pelotas choquen
    - Pausa (con barra espaciadora)
    - Traza de colores (la traza se mantiene)
    - Pelotas choquen con pared en el diametro no en el punto 0
    - Rebote aleatorio

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-/**
- * TODO:
- * 
- * - Pelotas choquen
- * - Pausa 
- * - Traza de colores (la traza se mantiene)
- * - Pelotas choquen con pared en el diametro no en el punto 0
- * 
- */
+
 let ball_q = 15;
 let ball_size = 10;
 let gridSize = 30;
@@ -14,6 +6,9 @@ let cellSize = 20;
 let dungeonData;
 let circles = [];
 let speedScale = 1; // Ajusta la velocidad global
+let isPaused = false;
+
+
 
 function preload() {
   // Cargar el JSON con la estructura de la mazmorra
@@ -28,22 +23,34 @@ function setup() {
   }
 }
 
+function keyPressed() {
+  if (keyCode === 32) {
+    isPaused = !isPaused;
+  }
+}
+
 function draw() {
-  background(0);
-  // Dibujar la mazmorra según el JSON
-  for (let y = 0; y < gridSize; y++) {
-    for (let x = 0; x < gridSize; x++) {
-      if (dungeonData.grid[y][x] === 1) {
-        fill(0);
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+  if (!isPaused) {
+    background(0);
+    // Dibujar la mazmorra según el JSON
+    for (let y = 0; y < gridSize; y++) {
+      for (let x = 0; x < gridSize; x++) {
+        if (dungeonData.grid[y][x] === 1) {
+          stroke("white");
+          fill(0);
+          rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        }
       }
     }
-  }
 
-  // Mover y dibujar los círculos
-  for (let circle of circles) {
-    circle.move();
-    circle.display();
+    // Mover y dibujar los círculos
+    for (let i = 0; i < circles.length; i++) {
+      circles[i].move();
+      circles[i].display();
+      for (let j = i + 1; j < circles.length; j++) {
+        circles[i].collide(circles[j]);
+      }
+    }
   }
 }
 
@@ -54,35 +61,62 @@ class Circle {
     this.color = color;
     this.speedX = random(-1, 1) * speedScale;
     this.speedY = random(-1, 1) * speedScale;
-    // this.prevX = x;
-    // this.prevY = y;
-    
+    this.history = [];
+
   }
 
   move() {
+    if (!isPaused) {
+      this.x += this.speedX;
+      this.y += this.speedY;
 
-    this.x += this.speedX;
-    this.y += this.speedY;
-
-    let gridX = floor(this.x / cellSize);
-    let gridY = floor(this.y / cellSize);
-    if (gridX >= 0 && gridX < gridSize && gridY >= 0 && gridY < gridSize) {
-      if (dungeonData.grid[gridY][gridX] === 1) {
-        // Colisión detectada
-        // Invertir la velocidad según la dirección de la colisión
-        if (this.speedX > 0) {
-          this.x = gridX * cellSize;
-        } else {
-          this.x = (gridX + 1) * cellSize;
+      let gridX = floor(this.x / cellSize);
+      let gridY = floor(this.y / cellSize);
+      if (gridX >= 0 && gridX < gridSize && gridY >= 0 && gridY < gridSize) {
+        if (dungeonData.grid[gridY][gridX] === 1) {
+          // Colisión detectada
+          // Invertir la velocidad según la dirección de la colisión
+          this.x = (this.speedX > 0) ? (gridX * cellSize) : ((gridX + 1) * cellSize);
+          this.y = (this.speedY > 0) ? (gridY * cellSize) : ((gridY + 1) * cellSize);
+          this.speedX *= -1;
+          this.speedY *= -1;
+          this.speedX += random(-0.5, 0.5);
+          this.speedY += random(-0.5, 0.5);
         }
-        if (this.speedY > 0) {
-          this.y = gridY * cellSize;
-        } else {
-          this.y = (gridY + 1) * cellSize;
-        }
-        this.speedX *= -1;
-        this.speedY *= -1;
       }
+      var v = createVector(this.x, this.y);
+      this.history.push(v);
+      this.displayTrail();
+    }
+  }
+
+  collide(other) {
+    let dx = this.x - other.x;
+    let dy = this.y - other.y;
+    let distance = sqrt(dx * dx + dy * dy);
+
+    if (distance < ball_size * 2) {
+      // Colisión detectada
+      let angle = Math.atan2(dy, dx);
+      let sin = Math.sin(angle);
+      let cos = Math.cos(angle);
+
+      // Rotar las velocidades de los círculos
+      let tempX = this.speedX * cos + this.speedY * sin;
+      let tempY = -this.speedX * sin + this.speedY * cos;
+      this.speedX = other.speedX * cos + other.speedY * sin;
+      this.speedY = -other.speedX * sin + other.speedY * cos;
+      other.speedX = tempX;
+      other.speedY = tempY;
+    }
+  }
+
+
+  displayTrail() {
+    for (var i = 0; i < this.history.length; i++) {
+      var pos = this.history[i];
+      fill(this.color);
+      ellipse(pos.x, pos.y, ball_size - (ball_size / 2));
     }
   }
 
@@ -90,10 +124,5 @@ class Circle {
     fill(this.color);
     noStroke();
     ellipse(this.x, this.y, ball_size);
-
-    // Dibujar la estela
-    stroke(this.color);
-    strokeWeight(2);
-    line(this.x, this.y,this.prevX, this.prevY, ); 
   }
 }
